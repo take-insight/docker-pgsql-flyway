65cac8fa954b2b455effb17602dc9e6904141fd0
yuichi.sano <yuichi.sano@insight-inc.biz>
2022-07-19 19:35:45 +0900

    バッチテーブル追加

--- flyway/src/main/resources/db/migration/V01/V01_000_000000000001__init.sql
+++ flyway/src/main/resources/db/migration/V01/V01_000_000000000001__init.sql
@@ -263,6 +263,14 @@ COMMENT ON COLUMN sample.merchant_x_users.created_at IS '登録日時';
 COMMENT ON COLUMN sample.merchant_x_users.created_user IS '登録者';
 
 
+
+--　
+CREATE TABLE sample.prefectures (
+    id integer NOT NULL,
+    name text
+);
+
+
 -- 拠点
 CREATE SEQUENCE sample.seq_work_place_id
     START WITH 1
@@ -299,6 +307,22 @@ CREATE TABLE sample.devices (
         ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
+
+
+create table sample.table_batch_audit (
+    id serial
+    , target_table_name text not null
+    , record_cnt integer not null
+    , before_record_cnt integer not null
+    , diff_cnt integer not null
+    , status CHAR(1) not null CHECK(status IN ('1', '2', '3'))
+    , apply_date timestamp with time zone not null
+    , implementation_date timestamp with time zone
+    , user_id text not null default CURRENT_USER
+    , create_date timestamp with time zone not null
+    , audit_date timestamp with time zone default now()
+    , constraint table_batch_audit_pkey primary key (id)
+) ;
 
 CREATE TABLE sample.yuseiooguchijigyoushoyubinbangous (
     jis character(5) NOT NULL,
@@ -438,4 +462,56 @@ GRANT EXECUTE ON FUNCTION sample.fnc_logged_audit() TO PUBLIC;
 CREATE TRIGGER trg_yuseiyubinbangous_z AFTER INSERT OR UPDATE OR DELETE
     ON sample.yuseiyubinbangous FOR EACH ROW
 EXECUTE PROCEDURE sample.fnc_logged_audit();
+
+
+INSERT INTO sample.prefectures
+VALUES
+    (1,'北海道'),
+    (2,'青森県'),
+    (3,'岩手県'),
+    (4,'宮城県'),
+    (5,'秋田県'),
+    (6,'山形県'),
+    (7,'福島県'),
+    (8,'茨城県'),
+    (9,'栃木県'),
+    (10,'群馬県'),
+    (11,'埼玉県'),
+    (12,'千葉県'),
+    (13,'東京都'),
+    (14,'神奈川県'),
+    (15,'新潟県'),
+    (16,'富山県'),
+    (17,'石川県'),
+    (18,'福井県'),
+    (19,'山梨県'),
+    (20,'長野県'),
+    (21,'岐阜県'),
+    (22,'静岡県'),
+    (23,'愛知県'),
+    (24,'三重県'),
+    (25,'滋賀県'),
+    (26,'京都府'),
+    (27,'大阪府'),
+    (28,'兵庫県'),
+    (29,'奈良県'),
+    (30,'和歌山県'),
+    (31,'鳥取県'),
+    (32,'島根県'),
+    (33,'岡山県'),
+    (34,'広島県'),
+    (35,'山口県'),
+    (36,'徳島県'),
+    (37,'香川県'),
+    (38,'愛媛県'),
+    (39,'高知県'),
+    (40,'福岡県'),
+    (41,'佐賀県'),
+    (42,'長崎県'),
+    (43,'熊本県'),
+    (44,'大分県'),
+    (45,'宮崎県'),
+    (46,'鹿児島県'),
+    (47,'沖縄県');
+
 
